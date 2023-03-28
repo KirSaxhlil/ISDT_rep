1049cb57363b956d66b1cab61eb2ea572f5c20bf
KirSaxhlil <dimapisarenko28@gmail.com>
2023-03-28 14:04:06 +0300

    hello world added comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+////// super comment //////
 int main() {
 	char name[20];
-	std::cout << "What's your name?" << std::endl;
-	std::cin >> name;
-	std::cout << "Hello world form " << name << std::endl;
+	std::cout << "What's your name?" << std::endl; // my comment
+	std::cin >> name; // stup id co mment
+	std::cout << "Hello world form " << name << std::endl; // another comment
 	return 0;
 }
