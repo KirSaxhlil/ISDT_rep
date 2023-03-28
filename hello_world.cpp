2db47727706f964132478e85859e70a7e146ffec
KirSaxhlil <dimapisarenko28@gmail.com>
2023-03-28 13:35:28 +0300

    hello world changed for deleting using

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-using namespace std;
 
 int main() {
 	char name[20];
-	cout << "What's your name?" << endl;
-	cin >> name;
-	cout << "Hello world form " << name << endl;
+	std::cout << "What's your name?" << std::endl;
+	std::cin >> name;
+	std::cout << "Hello world form " << name << std::endl;
 	return 0;
 }
